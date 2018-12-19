bcea716e2789e906606d00bb32808bc203a56ced
Tasan009St <orangebbumesak@gmail.com>
2018-12-20 01:37:05 +0900

    delete api key

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def index
     require 'google/apis/drive_v2'
     drive = Google::Apis::DriveV2::DriveService.new
-    drive.key = 'AIzaSyCDku6XekSwpF1Mjrml5ukMMp_F2JPaU4Q' 
+    drive.key = '' 
     # Search for files in Drive (first page only)
     @book = drive.list_files(q: "title contains 'finances'")
   end 
