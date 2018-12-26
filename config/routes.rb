18ccd7ea8847024b1ec902147aa3c675079ed742
Tasan009St <orangebbumesak@gmail.com>
2018-12-26 23:09:51 +0900

    amazon api申請用にinfoとhelpページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/help'
+  get 'static_pages/info'
   root 'top_pages#index'
   get '/books/new', to:'books#new'
   get '/books/index', to:'books#index'
